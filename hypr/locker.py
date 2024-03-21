9f761ae8d8657bbb1d786e7691683d7d03dbe4ce
prushton2 <peterrushton418@gmail.com>
2024-03-21 14:51:10 -0400

    gave up on fullscreen

--- hypr/locker.py
+++ hypr/locker.py
@@ -14,13 +14,16 @@ def main() {
         subprocess.check_output(["hyprctl", "dispatch", "workspace", str(99-i)])
         subprocess.Popen(["kitty", "/home/prushton/dotfiles/hypr/pipes.sh"])
         time.sleep(0.2)
-        subprocess.check_output(["hyprctl", "dispatch", "fullscreen"])
     }
-
+    
     subprocess.run(["dunstctl", "set-paused", "true"])
+    subprocess.run(["killall", "waybar"])
+    
     subprocess.run(["hyprlock"])
+    
     subprocess.run(["dunstctl", "set-paused", "false"])
-
+    subprocess.Popen(["waybar"])
+    
 
     for i in range(monitors) {
         subprocess.check_output(["hyprctl", "dispatch", "focusmonitor", str(i)])
