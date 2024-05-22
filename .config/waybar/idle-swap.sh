b4b8bf0af44100889511315ab5b2136cdcca0571
prushton2 <peterrushton418@gmail.com>
2024-05-22 19:15:20 -0400

    fixed sleep

--- .config/waybar/idle-swap.sh
+++ .config/waybar/idle-swap.sh
@@ -1,14 +1,11 @@
 is_active=$(systemctl status hypridle --user | grep 'Active: ' | sed -e 's/     Active: //' | sed -e 's/ (.*//')
 
-echo $is_active >~/.config/waybar/lastout
-
 if [[ $1 == "-s" ]]; then
 	if [[ $is_active = "active" ]]; then
 		systemctl stop hypridle --user
 	else
 		systemctl start hypridle --user
 	fi
-	sleep 1
 	kill -s SIGRTMIN+1 $(pidof waybar)
 fi
 
--- .config/waybar/monitor.sh
+++
@@ -1,6 +0,0 @@
-while [ 1 -eq 1 ]; do
-	sleep 1
-	tput reset
-	echo $(systemctl status hypridle --user | grep "Active: ")
-	cat lastout
-done
